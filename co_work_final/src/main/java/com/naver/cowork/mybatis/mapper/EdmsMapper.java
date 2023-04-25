7c86e1543fa437a1e11bcee9f42d43083375d29b
qwe79 <qwe79@DESKTOP-9KI5UIK>
2023-04-25 18:14:59 +0900

    기타

--- co_work_final/src/main/java/com/naver/cowork/mybatis/mapper/EdmsMapper.java
+++ co_work_final/src/main/java/com/naver/cowork/mybatis/mapper/EdmsMapper.java
@@ -1,19 +1,53 @@
 package com.naver.cowork.mybatis.mapper;
 
-import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.naver.cowork.domain.Edms;
+import com.naver.cowork.domain.ApprovalLine;
+import com.naver.cowork.domain.BsTrip;
+import com.naver.cowork.domain.Dept;
+import com.naver.cowork.domain.DocumentForm;
+import com.naver.cowork.domain.Member;
+
+import com.naver.cowork.domain.Calendar;
+import com.naver.cowork.domain.Criteria;
+import com.naver.cowork.domain.EDMS;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.HashMap;
+import java.util.List;
 
 
 @Mapper
-public interface EdmsMapper {
+public interface EDMSMapper {
 
-	public List<Edms> getEdmsList();
 
-	public List<Edms> getEdmsDetail();
+	int insertDocForm(DocumentForm docForm);
 
-	public Edms getApprovalOpinion(int apNumVal);
+	int insertApproval(ApprovalLine approval);
+
+	int insertBsTrip(BsTrip bst);
+
+	List<Member> getAllUsersInfo(String loginId);
+
+	List<Dept> deptList();
+
+	List<Member> getDeptMeberList(String dept_name);
+
+	Member getUsersInfo(String loginId);
+
+	int getSerialNumber();
+
+  public List<EDMS> getMyDoc(Criteria cri);
+    
+  public List<EDMS> getMyDocApp(Criteria cri);
+
+  public int getCountDoc(String user_id);
+
+  public int getCountDocApp(String user_id);
+
+  public List<EDMS> getAppLine(int document_no);
+
+
 
 }
--- co_work_final/src/main/java/com/naver/cowork/service/EdmsService.java
+++ co_work_final/src/main/java/com/naver/cowork/service/EdmsService.java
@@ -1,25 +1,39 @@
 package com.naver.cowork.service;
 
+
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
-import com.naver.cowork.domain.Edms;
+import com.naver.cowork.domain.BsTrip;
+import com.naver.cowork.domain.Dept;
+import com.naver.cowork.domain.Member;
+import com.naver.cowork.domain.ChatVO;
+import com.naver.cowork.domain.Criteria;
+import com.naver.cowork.domain.EDMS;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
-public interface EdmsService {
+public interface EDMSService {
 
-		// 글 목록
-	public List<Edms> getEdmsList();
+	public String insertBsTripForm(BsTrip bst);
 
-	public List<Edms> getEdmsDetail();
+	public List<Member> getAllUsersInfo(String loginId);
 
-	public Edms getApprovalOpinion(int apNumVal);
+	public List<Dept> getDeptList();
+
+	public Member getUsersInfo(String loginId);
 	
-	
-}
+	public String generateDocumentNumber();
+  public List<EDMS> getMyDoc(Criteria cri);
 
+  public List<EDMS> getMyDocApp(Criteria cri);
 
+  public int getCountDoc(String user_id);
 
-
+  public int getCountDocApp(String user_id);
+  public List<EDMS> getAppLine(int document_no);
